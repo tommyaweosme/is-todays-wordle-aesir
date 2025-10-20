b67f271ab943adfd7db5a88e180fb7afe53880f9
tommyaweosme <121325574+tommyaweosme@users.noreply.github.com>
2025-10-20 15:54:30 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const currentDate = new Date();
+const dayOfMonth = currentDate.getDate();
+function getWordle(){
+  return(answers[dayOfMonth]);
+};
+function checkWordle(x){
+  if (getWordle() == x){
+    return(true);
+  } else {
+    return(false);
+  };
+};
